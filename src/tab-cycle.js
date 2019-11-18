ec848031f59ad7bf5876f994d0b5a6ca3a0108b6
Stefan Houtzager <agent_008_nl@yahoo.com>
2019-11-18 12:39:31 +0100

    Create tab-cycle.js

---
+++ src/tab-cycle.js
@@ -0,0 +1,57 @@
+  const isTabPressed = e  => e.key === 'Tab' || e.keyCode === 9;
+
+  const focusableElements = (rootElement) => rootElement.querySelectorAll(
+    'a[href]:not([disabled]), ' +
+	  'button:not([disabled]), ' +
+  	'select:not([disabled]), ' +
+  	'[href], ' +
+  	'textarea:not([disabled]), ' +
+  	'input:not([disabled]), ' +
+	  'select:not([disabled]), ' +
+  	'[tabindex]:not([tabindex="-1"]');
+
+  const lastFocusableElements = (rootElement) =>
+    radioElementsOrElement(focusableElements(rootElement)[focusableElements(rootElement).length - 1]);
+
+  const lastFocusableElement = (rootElement) =>
+    getFirstElement(lastFocusableElements(rootElement));
+
+  const firstFocusableElements = (rootElement) =>
+    radioElementsOrElement(focusableElements(rootElement)[0]);
+
+  const firstFocusableElement = (rootElement) =>
+    getFirstElement(firstFocusableElements(rootElement));
+
+  const getFirstElement = (elements) =>
+    Array.isArray(elements) ? elements[0] : elements;
+
+  const radioElementsOrElement = (element) =>
+    element.type === 'radio' ?
+	  [...document.querySelectorAll('input[type="radio"]:not([disabled])')].filter(item => item.name === element.name) :
+	  element;
+
+  const focusableIsActive = (elements) =>
+    Array.isArray(elements) ? elements.includes(document.activeElement) : elements === document.activeElement;
+
+  const run = () => {
+	  document
+	  .querySelectorAll('dialog, body')
+	  .forEach(rootElement => rootElement.addEventListener('keydown', e => {
+      if (!isTabPressed(e)) {
+        return;
+      }
+
+      if (e.shiftKey) /* shift + tab */ {
+        if (focusableIsActive(firstFocusableElements(rootElement))) {
+  	      lastFocusableElement(rootElement).focus();
+          e.preventDefault();
+        }
+      } else /* tab */ {
+        if (focusableIsActive(lastFocusableElements(rootElement))) {
+          firstFocusableElement(rootElement).focus();
+          e.preventDefault();
+        }
+      }
+    }));
+  }
+  export {run};
